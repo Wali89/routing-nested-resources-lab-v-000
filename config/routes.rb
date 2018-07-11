265770eefed6104899deb3400eb7e787af392ae6
Wali Rahman <wali.rahman89@yahoo.com>
2018-07-11 21:52:02 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show]
   end
-  resources :songs
+  resources :songs do
+    resources :artists, only: [:index. :show]
+  end
 end
